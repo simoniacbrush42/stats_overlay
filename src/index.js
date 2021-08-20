29e5feac2e76873b6366fbbcaed716ee774508ca
simoniacbrush42 <simoniacbrush42@gmail.com>
2021-08-20 13:27:56 -0400

    got initial bw stat checking working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const fetch = require('node-fetch');
+const fs = require("fs");
+const { contextIsolated } = require('process');
+var follow = require('text-file-follower')
 
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -12,8 +16,12 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    transparent:true
-    
+    transparent:true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+    }
   });
   mainWindow.setMenuBarVisibility(false)
 
@@ -28,7 +36,7 @@ const createWindow = () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
-
+check_file();
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
@@ -42,9 +50,52 @@ app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
+    
     createWindow();
+    
   }
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+
+function check_file(){
+   
+
+
+
+
+
+  /*
+  const md5 = require('md5');
+  txt_path = '../../../../../../Users/simon/.lunarclient/offline/1.8/logs/latest.log'
+  console.log(txt_path)
+  console.log('Watching for file changes on ${txt_path}');
+  let md5Previous = null;
+  let fsWait = false;
+  
+  fs.watch(txt_path, (event , filename) => {
+    console.log("hi")
+    if(filename){
+      console.log("poo")
+        if(fsWait) return;
+        fsWait = setTimeout( () => {
+            fsWait = false;
+        }, 0.00000001);
+        const md5Current = md5(fs.readFileSync(txt_path));
+        if (md5Current === md5Previous) {
+            return;
+        }
+        md5Previous = md5Current;
+        console.log("file changed");
+        fs.readFile(txt_path, 'utf-8', (err, data) => {
+            if (err) throw err;
+            let lines = data.trim().split("\n")
+            console.log(lines[lines.length - 1])
+        })
+    }
+})
+*/
+}
+
